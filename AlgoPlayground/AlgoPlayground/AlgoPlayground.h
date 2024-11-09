e26d83e4794ae5bc1a0ed3dd2b5cd241d1fd1f6a
Fernando Putallaz <ferhedio@gmail.com>
2024-11-09 17:33:27 -0300

    Project Creation

---
+++ AlgoPlayground/AlgoPlayground/AlgoPlayground.h
@@ -0,0 +1,18 @@
+//
+//  AlgoPlayground.h
+//  AlgoPlayground
+//
+//  Created by Fernando Putallaz on 09/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlgoPlayground.
+FOUNDATION_EXPORT double AlgoPlaygroundVersionNumber;
+
+//! Project version string for AlgoPlayground.
+FOUNDATION_EXPORT const unsigned char AlgoPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgoPlayground/PublicHeader.h>
+
+
