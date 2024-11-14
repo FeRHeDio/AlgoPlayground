dc5889c822fd218a6624628711ebe271789ab095
Fernando Putallaz <ferhedio@gmail.com>
2024-11-14 08:56:31 -0300

    Set the table for `TwoNumberSum` problem

---
+++ AlgoPlayground/AlgoPlaygroundTests/TwoNumberSum.swift
@@ -0,0 +1,20 @@
+//
+//  TwoNumberSum.swift
+//  AlgoPlaygroundTests
+//
+//  Created by Fernando Putallaz on 14/11/2024.
+//
+
+import XCTest
+
+final class TwoNumberSum: XCTestCase {
+
+    
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
