4f0f608b9ae185b07b6f23721d0c0498ecd51b86
Fernando Putallaz <ferhedio@gmail.com>
2024-11-14 09:17:00 -0300

    Solve `TwoNumberSum` with 2 for loops -> not that efficient

--- AlgoPlayground/AlgoPlaygroundTests/TwoNumberSum.swift
+++ AlgoPlayground/AlgoPlaygroundTests/TwoNumberSum.swift
@@ -7,8 +7,37 @@
 
 import XCTest
 
-final class TwoNumberSum: XCTestCase {
+class TwoNumberSumEngine {
+    func findNumbersTwoForLoops(array: [Int], target: Int) -> [Int] {
+        var result = [Int]()
+        
+        for i in array {
+            for y in array {
+                if i + y == target {
+                    result.append(i)
+                    result.append(y)
+                    return result
+                }
+            }
+        }
+        
+        return []
+    }
+ }
 
+final class TwoNumberSum: XCTestCase {
+    
+    func test_twoNumberSum_fiundNumbersWithTwoForLoops() {
+        let sut = TwoNumberSumEngine()
+        
+        let array = [1, -9, -1, 11, 21]
+        let target = 10
+        
+        let result = sut.findNumbersTwoForLoops(array: array, target: target)
+        
+        XCTAssertEqual(result, [-1, 11])
+    }
+    
     
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
