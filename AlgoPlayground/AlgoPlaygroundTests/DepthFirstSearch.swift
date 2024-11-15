08c37185c43ef0b359e0e20c76500a2d9f943a75
Fernando Putallaz <ferhedio@gmail.com>
2024-11-15 09:45:58 -0300

    Set the table for `Depth-first Search` algo

---
+++ AlgoPlayground/AlgoPlaygroundTests/DepthFirstSearch.swift
@@ -0,0 +1,30 @@
+//
+//  DepthFirstSearch.swift
+//  AlgoPlaygroundTests
+//
+//  Created by Fernando Putallaz on 15/11/2024.
+//
+
+/*
+    ** Prompt **
+ 
+    You're given a `Node` class that has a `name` and an array of optional `children` nodes. When put together, nodes from an acyclic tree-like strcucture.
+ 
+    Implement the `deptFirstSearch` method on the `Node` class, which takes in an array, traverses the tree using the Depth-frist Search approach.
+    (Specifically navigatin the tree from left to right), stores all of the node's names in the input array, and returns it.
+ 
+ */
+
+import XCTest
+
+class DepthFirstSearchEngine {
+    
+    
+    
+}
+
+final class DepthFirstSearch: XCTestCase {
+
+    
+
+}
