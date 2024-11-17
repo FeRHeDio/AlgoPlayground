2977ab3074f1d803f7ea76a153ac7481f8b6918c
Fernando Putallaz <ferhedio@gmail.com>
2024-11-17 12:44:24 -0300

    Add detailed Case

--- AlgoPlayground/AlgoPlaygroundTests/DepthFirstSearch.swift
+++ AlgoPlayground/AlgoPlaygroundTests/DepthFirstSearch.swift
@@ -46,6 +46,16 @@ class DepthFirstSearchEngine {
 }
 
 final class DepthFirstSearch: XCTestCase {
+    
+//    graph =
+//            A
+//         /  |  \
+//        B   C   D
+//       / \     / \
+//      E   F   G   H
+//         / \   \
+//        I   J   K
+    
     func test_DFS_initialCase() {
         let graph = DepthFirstSearchEngine.Node(name: "A")
         
@@ -58,5 +68,47 @@ final class DepthFirstSearch: XCTestCase {
         var arrayToReturn = [String]()
         
         XCTAssertEqual(["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"], graph.depthFisrtSearch(array: &arrayToReturn))
+    }
+    
+//    graph =
+//              A
+//         /    |    \
+//        B     C     D
+//       / \   / \   / \
+//      E   F G   H I   J
+//         / \           \
+//        K   L           LL
+    
+    func test_DFS_detailedCase() {
+        let graph = DepthFirstSearchEngine.Node(name: "A")
+        
+        _ = graph
+            .addChild(name: "B") // Children 0
+            .addChild(name: "C") // Children 1
+            .addChild(name: "D") // Children 2
+        
+        _ = graph.children[0] // B
+            .addChild(name: "E")
+            .addChild(name: "F")
+        
+        _ = graph.children[0].children[1] // E
+            .addChild(name: "K")
+            .addChild(name: "L")
+        
+        _ = graph.children[1] // C
+            .addChild(name: "G")
+            .addChild(name: "H")
+        
+        _ = graph.children[2]
+            .addChild(name: "I")
+            .addChild(name: "J")
+        
+        _ = graph.children[2]
+            .children[1]
+            .addChild(name: "LL")
+        
+        var arrayToReturn = [String]()
+        
+        XCTAssertEqual(["A", "B", "E", "F", "K", "L", "C", "G", "H", "D", "I", "J", "LL"], graph.depthFisrtSearch(array: &arrayToReturn))
     }
 }
