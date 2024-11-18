1a66a763c6804657aa16e537169dd60e51ada626
Fernando Putallaz <ferhedio@gmail.com>
2024-11-18 11:07:04 -0300

    Add `LinkedListRemoveDuplicate` problem `Prompt`

--- AlgoPlayground/AlgoPlaygroundTests/LinkedListRemoveDuplicate.swift
+++ AlgoPlayground/AlgoPlaygroundTests/LinkedListRemoveDuplicate.swift
@@ -5,11 +5,24 @@
 //  Created by Fernando Putallaz on 18/11/2024.
 //
 
+/*
+    ** Prompt **
+ 
+    You're given the head of a Singly Linked List whose nodes are in sorted order with respect to their values.
+    Write a function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values.
+ 
+    The linked list should be modified in place (i.e., you shouldn't create a brand new Linked List), and the modified should still have its
+    nodes sorted with respect to their values.
+ 
+    Each `LinkedList` node has an integer `value` as well as a `next` node pointing to the next node in the list or `non/nil` if it's the tail
+    of the list.
+ 
+ */
+
+
 import XCTest
 
 class LinkedListRemoveDuplicateEngine {
-    
-    
 }
 
 final class LinkedListRemoveDuplicate: XCTestCase {
