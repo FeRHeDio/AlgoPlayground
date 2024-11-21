46ba9f2040f78a23df7761776072483f53db1ec5
Fernando Putallaz <ferhedio@gmail.com>
2024-11-21 10:18:55 -0300

    Remove `LinkedList` JSON code/decode helpers and test cases.

--- AlgoPlayground/AlgoPlaygroundTests/LinkedListRemoveDuplicate.swift
+++ AlgoPlayground/AlgoPlaygroundTests/LinkedListRemoveDuplicate.swift
@@ -60,66 +60,6 @@ class LinkedListRemoveDuplicateEngine {
 
 
 final class LinkedListRemoveDuplicate: XCTestCase {
-    func test_linkedList_noChangesReturnSame() {
-        let inputJSON: [String: Any] = [
-            "head": "0",
-            "nodes": [
-                ["id": "0", "next": "1", "value": 1],
-                ["id": "1", "next": "2", "value": 2],
-                ["id": "2", "next": nil, "value": 3]
-            ]
-        ]
-        
-        let inputList = LinkedListsTestUtils.linkedListFromJSON(inputJSON)
-        
-        let sut = makeSUT(head: inputList!)
-        
-        sut.printList(inputList)
-        
-        let result = sut.removeDuplicates(inputList!)
-        let resultJSON = LinkedListsTestUtils.linkedListToJSON(result)
-        
-        XCTAssertEqual(resultJSON as NSDictionary, inputJSON as NSDictionary)
-        
-    }
-    
-    func test_removeDuplicates_returnCleanedList() {
-        let inputJSON: [String: Any] = [
-            "head": "0",
-            "nodes": [
-                ["id": "0", "next": "1", "value": 1],
-                ["id": "1", "next": "2", "value": 1],
-                ["id": "2", "next": "3", "value": 2],
-                ["id": "3", "next": "4", "value": 3],
-                ["id": "4", "next": "5", "value": 3],
-                ["id": "5", "next": "6", "value": 3],
-                ["id": "6", "next": "7", "value": 3],
-                ["id": "7", "next": "8", "value": 3],
-                ["id": "8", "next": "9", "value": 3],
-                ["id": "9", "next": nil, "value": 3]
-            ]
-        ]
-        
-        let expectedResult: [String: Any] = [
-            "head": "0",
-            "nodes": [
-                ["id": "0", "next": "1", "value": 1],
-                ["id": "1", "next": "2", "value": 2],
-                ["id": "2", "next": nil, "value": 3]
-            ]
-        ]
-        
-        let inputList = LinkedListsTestUtils.linkedListFromJSON(inputJSON)
-        
-        let sut = makeSUT(head: inputList!)
-        
-        let result = sut.removeDuplicates(inputList!)
-        let outputJSON = LinkedListsTestUtils.linkedListToJSON(result)
-        
-        XCTAssertEqual(outputJSON as NSDictionary, expectedResult as NSDictionary, "The linked list was not removing ocurrences correctly.")
-        
-    }
-    
     func test_linkedList_removeDuplicatesReturnCleanedList() {
         let linkedList = createLinkedList(from: [1, 3, 3, 5, 5, 9])
         let sut = makeSUT(head: linkedList)
@@ -149,6 +89,11 @@ final class LinkedListRemoveDuplicate: XCTestCase {
     
     // MARK: - Helpers
     
+    typealias LL = LinkedListRemoveDuplicateEngine.LinkedList
+    
+    private func makeSUT(head: LL) -> LL {
+        LL(value: head.value)
+    }
     
     private func createLinkedList(from values: [Int]) -> LinkedListRemoveDuplicateEngine.LinkedList {
         guard !values.isEmpty else {
@@ -177,84 +122,5 @@ final class LinkedListRemoveDuplicate: XCTestCase {
         }
         
         return array
-    }
-    
-    
-    typealias LL = LinkedListRemoveDuplicateEngine.LinkedList
-    
-    private func makeSUT(head: LL) -> LL {
-        LL(value: head.value)
-    }
-    
-    private class LinkedListsTestUtils {
-        static func linkedListFromJSON(_ dict: [String: Any]) -> LL? {
-            guard
-                let nodes = dict["nodes"] as? [[String: Any]],
-                let headId = dict["head"] as? String
-            else { return nil }
-            
-            var nodeMap: [String: LL] = [:]
-            for node in nodes {
-                if let id = node["id"] as? String, let value = node["value"] as? Int {
-                    nodeMap[id] = LL(value: value)
-                }
-            }
-            
-            for node in nodes {
-                if let id = node["id"] as? String,
-                   let nextId = node["next"] as? String,
-                   let currentNode = nodeMap[id] {
-                    currentNode.next = nodeMap[nextId]
-                }
-            }
-            
-            return nodeMap[headId]
-        }
-        
-        static func linkedListToJSON(_ head: LL) -> [String: Any] {
-            var nodes: [[String: Any]] = []
-            var nodeMap = [ObjectIdentifier: String]()
-            var currentNode: LL? = head
-            var idCounter = 0
-
-            while let node = currentNode {
-                let objectId = ObjectIdentifier(node)
-                let id: String
-                
-                if let existingId = nodeMap[objectId] {
-                    id = existingId
-                } else {
-                    id = "\(idCounter)"
-                    nodeMap[objectId] = id
-                    idCounter += 1
-                }
-
-                let nextID = node.next.flatMap { nextNode in
-                    let nextObjectId = ObjectIdentifier(nextNode)
-                    if let existingNextId = nodeMap[nextObjectId] {
-                        return existingNextId
-                    } else {
-                        let newNextId = "\(idCounter)"
-                        nodeMap[nextObjectId] = newNextId
-                        idCounter += 1
-                        return newNextId
-                    }
-                }
-
-                nodes.append([
-                    "id": id,
-                    "value": node.value,
-                    "next": nextID ?? NSNull()
-                ])
-
-                currentNode = node.next
-            }
-
-            return [
-                "head": nodeMap[ObjectIdentifier(head)] ?? "0",
-                "nodes": nodes
-            ]
-        }
-
     }
 }
