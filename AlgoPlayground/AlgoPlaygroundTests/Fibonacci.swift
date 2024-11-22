81f044de4a708e463db819d8ac7d6eee7944a11f
Fernando Putallaz <ferhedio@gmail.com>
2024-11-22 10:08:21 -0300

    Set the table for Fibonacci sequence. Finding the Nth Fibonacci number

---
+++ AlgoPlayground/AlgoPlaygroundTests/Fibonacci.swift
@@ -0,0 +1,31 @@
+//
+//  Fibonacci.swift
+//  AlgoPlaygroundTests
+//
+//  Created by Fernando Putallaz on 22/11/2024.
+//
+
+/*
+    ** Prompt **
+    
+    The Fibonacci Sequence is defined as follos: The first number of the sequence is 0, the second number is 1, and the nth number is the sum of the (n-1)th and (n-2)nth numbers.
+    
+    Write a function that takes in an integer n and returns the Nth Fibonacci number.
+ 
+    Important Note: The Fibonacci sequence is often defined with its first two numbers as F0 = 0 and F1 = 1.
+    For the purpose of this question, the first Fibonacci number is F0; therefore, getNthFib(1) is equal to F0, getNthFib(2) is equal to F1, and so on.
+    
+ */
+
+import XCTest
+
+class FibonacciEngine {
+    
+    
+}
+
+final class Fibonacci: XCTestCase {
+
+   
+
+}
