50159c7e688705321333ea35ed710354cfcbbc3e
Fernando Putallaz <ferhedio@gmail.com>
2024-11-23 10:07:17 -0300

    First approach for the Fibonacci Sequence.. naive approach

--- AlgoPlayground/AlgoPlaygroundTests/Fibonacci.swift
+++ AlgoPlayground/AlgoPlaygroundTests/Fibonacci.swift
@@ -8,7 +8,7 @@
 /*
     ** Prompt **
     
-    The Fibonacci Sequence is defined as follos: The first number of the sequence is 0, the second number is 1, and the nth number is the sum of the (n-1)th and (n-2)nth numbers.
+    The Fibonacci Sequence is defined as follows: The first number of the sequence is 0, the second number is 1, and the nth number is the sum of the (n-1)th and (n-2)nth numbers.
     
     Write a function that takes in an integer n and returns the Nth Fibonacci number.
  
@@ -21,10 +21,35 @@ import XCTest
 
 class FibonacciEngine {
     
-    
-}
+    func getNthFib(n: Int) -> Int {
+        
+        if n == 2 {
+            return 1
+        } else if n == 1 {
+            return 0
+        } else {
+            return getNthFib(n: n - 1) + getNthFib(n: n - 2)
+        }
+    }
+ }
 
 final class Fibonacci: XCTestCase {
+    
+    func test_fibonacci_takesTwoReturnsOne() {
+        let sut = FibonacciEngine()
+        
+        let result = sut.getNthFib(n: 2)
+        
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_fibonacci_takesOneReturnsZero() {
+        let sut = FibonacciEngine()
+        
+        let result = sut.getNthFib(n: 1)
+        
+        XCTAssertEqual(result, 0)
+    }
 
    
 
