65963cd75985fb0d52262848c8e56fc02029aec6
Fernando Putallaz <ferhedio@gmail.com>
2024-11-24 23:11:48 -0300

    Add Prompt

--- AlgoPlayground/AlgoPlaygroundTests/ClassPhoto.swift
+++ AlgoPlayground/AlgoPlaygroundTests/ClassPhoto.swift
@@ -5,6 +5,25 @@
 //  Created by Fernando Putallaz on 24/11/2024.
 //
 
+/*
+    ** Prompt **
+ 
+    It's photo day at the school, and you're the photographer assigned to take the photos.
+    The class that you'll be photographing has an even number of students, and all this students are wearing red or blue shirts.
+    In fact, half of the class is wearing red shirts, and the other half is wearing blue shirts. You're responsible for arranging the
+    students in two rows before taking the photo. Each row should contain the same number of students and should adhere to the following
+    guidelines.
+ 
+    - All Students wearing red shirts must be in the same row.
+    - All Students wearing blue shirts must be in the same row.
+    - Each Student in the back must be strictly taller than the Student directly in fron of them in the fron row.
+ 
+    You're given two input arrays: One containing the heights of all students with red shirts and another one containing all the students
+    with blue shirts. Thus array would be the same length. All heights will be positive integers. Assume that each class has at least 2 Students.
+ 
+    Return wheter or not the class photo following this guideines can be taken.
+ 
+ */
 import XCTest
 
 class ClassPhotoEngine {
