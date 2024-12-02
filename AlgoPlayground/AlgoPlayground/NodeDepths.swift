0b62e81bfa75724bd833739d045b38e77f041b55
Fernando Putallaz <ferhedio@gmail.com>
2024-12-01 22:18:45 -0300

    Add prompt for `Node Depths` problem

--- AlgoPlayground/AlgoPlayground/NodeDepths.swift
+++ AlgoPlayground/AlgoPlayground/NodeDepths.swift
@@ -5,6 +5,19 @@
 //  Created by Fernando Putallaz on 01/12/2024.
 //
 
+/*
+    ** Prompt **
+ 
+    The distance between a a node in a Binary Tree and the tree's node is called the node's depth.
+ 
+    Write a function that takes in a Binary Tree and returns the sum of its node's depths.
+ 
+    Each `Binary Tree` node has an integer `value`, a `left`, and a `right` child node.
+ 
+    Children nodes can be `BinaryTree` nodes themselves or non/nil.
+ 
+ */
+
 import XCTest
 
 class NodeDepthsEngine {
