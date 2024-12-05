f45d478b81c62660913f6fcb757f0b4fe032ab56
Fernando Putallaz <ferhedio@gmail.com>
2024-12-05 09:10:23 -0300

    Add Prompt for `Caesar Cipher` problem

--- AlgoPlayground/AlgoPlaygroundTests/Strings/CaesarCipherEncryptor.swift
+++ AlgoPlayground/AlgoPlaygroundTests/Strings/CaesarCipherEncryptor.swift
@@ -5,6 +5,18 @@
 //  Created by Fernando Putallaz on 05/12/2024.
 //
 
+/*
+    ** Prompt **
+ 
+    Given a non-empty string of lowercase letters and a non-negative integer representing a key,
+    write a function that retursn a new string obtained by shifting every letter in the input string by k positions
+    in the alphabet, where k is the key.
+ 
+    Not that letter should "wrap" around the alphabet; in other words, the letter `z` shifted by one returns the
+    letter `a`.
+    
+ */
+
 import XCTest
 
 class CaesarCipher {
