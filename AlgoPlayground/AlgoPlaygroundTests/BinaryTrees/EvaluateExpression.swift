093b5b4713748beb7d56315bc01458dc54e0da83
Fernando Putallaz <ferhedio@gmail.com>
2024-12-05 06:54:57 -0300

    Add test for `Division Rounded`

--- AlgoPlayground/AlgoPlaygroundTests/BinaryTrees/EvaluateExpression.swift
+++ AlgoPlayground/AlgoPlaygroundTests/BinaryTrees/EvaluateExpression.swift
@@ -97,6 +97,16 @@ final class EvaluateExpression: XCTestCase {
         XCTAssertEqual(sut.evaluate(tree), 5)
     }
     
+    func test_evaluateExpression_returnsDivisionRounded() {
+        let sut = makeSUT()
+        
+        let node3 = TreeNode(id: "3", value: 2)
+        let node2 = TreeNode(id: "2", value: 11)
+        let tree = TreeNode(id: "1", value: -3, left: node2, right: node3)
+        
+        XCTAssertEqual(sut.evaluate(tree), 5)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> EvaluateExpressionEngine {
