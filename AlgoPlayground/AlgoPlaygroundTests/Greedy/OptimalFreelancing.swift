284fd09779c9a28d1da1e426feb7a44eb72e7521
Fernando Putallaz <ferhedio@gmail.com>
2024-12-09 10:22:06 -0300

    Remove warnings in `OptimalFreelancing` but `for_where`

--- AlgoPlayground/AlgoPlaygroundTests/Greedy/OptimalFreelancing.swift
+++ AlgoPlayground/AlgoPlaygroundTests/Greedy/OptimalFreelancing.swift
@@ -14,12 +14,14 @@
  
     Given this information, write a function that returns the maximum profit that can be obtained in a 7 day period.
  
-    Each job will take a full day to complete, and the deadline will be given as the numbers of days left to complete the job.
+    Each job will take a full day to complete, and the deadline will be given as the numbers of days left 
+    to complete the job.
  
     For example, if a job has a deadline of 1, then it can only be completed if it is the first job worked on.
     If a job has a deadline of 2, then it could be started on the first or second day.
  
-    Note: There is no requirement to complete all of the jobs. Only one job can be worked on at a time, meaning that in some
+    Note: There is no requirement to complete all of the jobs. Only one job can be worked on at a time, meaning 
+    that in some
     scenarios it will be impossible to complete them all.
     
  */
@@ -47,6 +49,8 @@ class OptimalFreelancingEngine {
             for time in stride(from: maxTime - 1, through: 0, by: -1) {
                 
                 // If a day is available (false), it is marked as booked (true), and the payment is added to the profit.
+                // TODO: Fix Warning (for_where)
+                
                 if !timeline[time] {
                     timeline[time] = true
                     profit += payment
@@ -60,7 +64,6 @@ class OptimalFreelancingEngine {
     }
 }
 
-
 final class OptimalFreelancing: XCTestCase {
     func test_optimalFreelancing_returns5() {
         let sut = makeSUT()
@@ -68,7 +71,7 @@ final class OptimalFreelancing: XCTestCase {
         let jobs = [
             ["deadline": 8, "payment": 3],
             ["deadline": 1, "payment": 1],
-            ["deadline": 1, "payment": 2],
+            ["deadline": 1, "payment": 2]
         ]
         
         let result = sut.calculateOptimal(jobs)
