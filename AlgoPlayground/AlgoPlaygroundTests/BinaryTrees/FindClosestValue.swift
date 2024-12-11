174f52494eabe3d76bcf0ade2f06570945fa94a6
Fernando Putallaz <ferhedio@gmail.com>
2024-12-11 08:51:50 -0300

    Add prompt for `Find Closest Value` in BST

--- AlgoPlayground/AlgoPlaygroundTests/BinaryTrees/FindClosestValue.swift
+++ AlgoPlayground/AlgoPlaygroundTests/BinaryTrees/FindClosestValue.swift
@@ -5,6 +5,22 @@
 //  Created by Fernando Putallaz on 11/12/2024.
 //
 
+/*
+    ** Prompt **
+    
+    Write a function that takes in a Binary Search Tree (BST) and a target integer value
+    and returns the closest value to that target value contained in the BST.
+ 
+    You can assume that there will only be one closest value.
+ 
+    Each `BST` node has an integer `value`, a `left` child node, and a `right` child node.
+    A node is said to be a valid `BST` node if and only if it satisfies the BST property
+    which is: its `value` is strictly greater than the values of every node to its left;
+    its `value` is less than or equal to the values of every node to its right; and its
+    children nodes are either valid `BST` nodes themselves or `None/nil`.
+ 
+ */
+
 import XCTest
 
 class ClosestValueEngine {
